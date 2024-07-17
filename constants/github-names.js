2583f34338f1c4fa945a4bcc30f346b2a6b96009
iamkanguk97 <iamkanguk97@kakao.com>
2024-07-17 13:03:46 +0900

    Feat: DIL과 Presentation 자동 생성 script 작성 (미완료)

---
+++ constants/github-names.js
@@ -0,0 +1,13 @@
+/** GitHub Nicknames of our study */
+export default githubNames = [
+  "iamkanguk97",
+  "paran22",
+  "paigekim29",
+  "BeomyeonAndrewKim",
+  "she0108",
+  "nsong113",
+  "jrary",
+  "HyoKyoungLee",
+  "w00khyung",
+  "Yoonji23",
+];
